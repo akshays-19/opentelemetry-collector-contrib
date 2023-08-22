akshays-19/opentelemetry-collector-contrib

commit fac35eb94127357b681e743aaf1cc07728d02ace

[chore][fileconsumer] Remove readerBuilder and simplify reader instantiation (#25847)
Followup to
https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/25828

- Remove the `readerBuilder` entirely and instead works with clearer
`readerFactory` methods.
- Fixes test setup in `otlpjsonfilereceiver`, which was previous relying
on a force flush event. This was exposed as part of this refactoring.

pkg/stanza/fileconsumer/reader_factory.go
@@ -4,7 +4,6 @@
 package fileconsumer // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/fileconsumer"
 
 import (
-	"bufio"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -29,126 +28,93 @@ type readerFactory struct {
 }
 
 func (f *readerFactory) newReader(file *os.File, fp *fingerprint.Fingerprint) (*reader, error) {
-	return readerBuilder{
-		readerFactory: f,
-		file:          file,
-		readerMetadata: &readerMetadata{
-			Fingerprint:    fp,
-			FileAttributes: map[string]any{},
-		},
-	}.build()
-}
-
-func (f *readerFactory) newFromMetadata(m *readerMetadata) (*reader, error) {
-	return readerBuilder{
-		readerFactory:  f,
-		readerMetadata: m,
-	}.build()
+	return f.build(file, &readerMetadata{
+		Fingerprint:    fp,
+		FileAttributes: map[string]any{},
+	})
 }
 
 // copy creates a deep copy of a reader
 func (f *readerFactory) copy(old *reader, newFile *os.File) (*reader, error) {
-	return readerBuilder{
-		readerFactory: f,
-		file:          newFile,
-		splitFunc:     old.lineSplitFunc,
-		readerMetadata: &readerMetadata{
-			Fingerprint:     old.Fingerprint.Copy(),
-			Offset:          old.Offset,
-			FileAttributes:  util.MapCopy(old.FileAttributes),
-			HeaderFinalized: old.HeaderFinalized,
-		},
-	}.build()
+	return f.build(newFile, &readerMetadata{
+		Fingerprint:     old.Fingerprint.Copy(),
+		Offset:          old.Offset,
+		FileAttributes:  util.MapCopy(old.FileAttributes),
+		HeaderFinalized: old.HeaderFinalized,
+	})
 }
 
 func (f *readerFactory) newFingerprint(file *os.File) (*fingerprint.Fingerprint, error) {
 	return fingerprint.New(file, f.readerConfig.fingerprintSize)
 }
 
-type readerBuilder struct {
-	*readerFactory
-	file           *os.File
-	readerMetadata *readerMetadata
-	splitFunc      bufio.SplitFunc
-}
-
-func (b readerBuilder) build() (r *reader, err error) {
+func (f *readerFactory) build(file *os.File, m *readerMetadata) (r *reader, err error) {
 	r = &reader{
-		readerConfig:   b.readerConfig,
-		readerMetadata: b.readerMetadata,
+		readerConfig:   f.readerConfig,
+		readerMetadata: m,
+		file:           file,
+		SugaredLogger:  f.SugaredLogger.With("path", file.Name()),
+		decoder:        helper.NewDecoder(f.encoding),
 	}
 
-	if b.splitFunc != nil {
-		r.lineSplitFunc = b.splitFunc
-	} else {
-		r.lineSplitFunc, err = b.splitterFactory.Build(b.readerConfig.maxLogSize)
-		if err != nil {
+	if !f.fromBeginning {
+		if err = r.offsetToEnd(); err != nil {
 			return nil, err
 		}
 	}
 
-	r.decoder = helper.NewDecoder(b.encoding)
+	r.lineSplitFunc, err = f.splitterFactory.Build(f.readerConfig.maxLogSize)
+	if err != nil {
+		return nil, err
+	}
 
-	if b.headerConfig == nil || b.readerMetadata.HeaderFinalized {
+	if f.headerConfig == nil || m.HeaderFinalized {
 		r.splitFunc = r.lineSplitFunc
-		r.processFunc = b.readerConfig.emit
+		r.processFunc = f.readerConfig.emit
 	} else {
-		r.splitFunc = b.headerConfig.SplitFunc
-		r.headerReader, err = header.NewReader(b.SugaredLogger, *b.headerConfig)
+		r.splitFunc = f.headerConfig.SplitFunc
+		r.headerReader, err = header.NewReader(f.SugaredLogger, *f.headerConfig)
 		if err != nil {
 			return nil, err
 		}
 		r.processFunc = r.headerReader.Process
 	}
 
-	if b.file == nil {
-		return r, nil
-	}
-
-	r.file = b.file
-	r.SugaredLogger = b.SugaredLogger.With("path", b.file.Name())
-
 	// Resolve file name and path attributes
-	resolved := b.file.Name()
+	resolved := file.Name()
 
 	// Dirty solution, waiting for this permanent fix https://github.com/golang/go/issues/39786
 	// EvalSymlinks on windows is partially working depending on the way you use Symlinks and Junctions
 	if runtime.GOOS != "windows" {
-		resolved, err = filepath.EvalSymlinks(b.file.Name())
+		resolved, err = filepath.EvalSymlinks(file.Name())
 		if err != nil {
-			b.Errorf("resolve symlinks: %w", err)
+			f.Errorf("resolve symlinks: %w", err)
 		}
 	}
 	abs, err := filepath.Abs(resolved)
 	if err != nil {
-		b.Errorf("resolve abs: %w", err)
+		f.Errorf("resolve abs: %w", err)
 	}
 
-	if b.readerConfig.includeFileName {
-		r.FileAttributes[logFileName] = filepath.Base(b.file.Name())
+	if f.readerConfig.includeFileName {
+		r.FileAttributes[logFileName] = filepath.Base(file.Name())
 	} else if r.FileAttributes[logFileName] != nil {
 		delete(r.FileAttributes, logFileName)
 	}
-	if b.readerConfig.includeFilePath {
-		r.FileAttributes[logFilePath] = b.file.Name()
+	if f.readerConfig.includeFilePath {
+		r.FileAttributes[logFilePath] = file.Name()
 	} else if r.FileAttributes[logFilePath] != nil {
 		delete(r.FileAttributes, logFilePath)
 	}
-	if b.readerConfig.includeFileNameResolved {
+	if f.readerConfig.includeFileNameResolved {
 		r.FileAttributes[logFileNameResolved] = filepath.Base(abs)
 	} else if r.FileAttributes[logFileNameResolved] != nil {
 		delete(r.FileAttributes, logFileNameResolved)
 	}
-	if b.readerConfig.includeFilePathResolved {
+	if f.readerConfig.includeFilePathResolved {
 		r.FileAttributes[logFilePathResolved] = abs
 	} else if r.FileAttributes[logFilePathResolved] != nil {
 		delete(r.FileAttributes, logFilePathResolved)
-	}
-
-	if !b.fromBeginning {
-		if err = r.offsetToEnd(); err != nil {
-			return nil, err
-		}
 	}
 
 	return r, nil

pkg/stanza/fileconsumer/file.go
@@ -379,12 +379,9 @@ func (m *Manager) loadLastPollFiles(ctx context.Context) error {
 				m.Errorw("migrate header attributes: unexpected format")
 			}
 		}
-		r, err := m.readerFactory.newFromMetadata(rmd)
-		if err != nil {
-			m.Errorw("load reader", err)
-		} else {
-			m.knownFiles = append(m.knownFiles, r)
-		}
+
+		// This reader won't be used for anything other than metadata reference, so just wrap the metadata
+		m.knownFiles = append(m.knownFiles, &reader{readerMetadata: rmd})
 	}
 
 	return nil

receiver/otlpjsonfilereceiver/file_test.go
@@ -51,11 +51,12 @@ func TestFileTracesReceiver(t *testing.T) {
 	marshaler := &ptrace.JSONMarshaler{}
 	b, err := marshaler.MarshalTraces(td)
 	assert.NoError(t, err)
+	b = append(b, '\n')
 	err = os.WriteFile(filepath.Join(tempFolder, "traces.json"), b, 0600)
 	assert.NoError(t, err)
 	time.Sleep(1 * time.Second)
-	require.Len(t, sink.AllTraces(), 1)
 
+	require.Len(t, sink.AllTraces(), 1)
 	assert.EqualValues(t, td, sink.AllTraces()[0])
 	err = receiver.Shutdown(context.Background())
 	assert.NoError(t, err)
@@ -77,6 +78,7 @@ func TestFileMetricsReceiver(t *testing.T) {
 	marshaler := &pmetric.JSONMarshaler{}
 	b, err := marshaler.MarshalMetrics(md)
 	assert.NoError(t, err)
+	b = append(b, '\n')
 	err = os.WriteFile(filepath.Join(tempFolder, "metrics.json"), b, 0600)
 	assert.NoError(t, err)
 	time.Sleep(1 * time.Second)
@@ -103,6 +105,7 @@ func TestFileLogsReceiver(t *testing.T) {
 	marshaler := &plog.JSONMarshaler{}
 	b, err := marshaler.MarshalLogs(ld)
 	assert.NoError(t, err)
+	b = append(b, '\n')
 	err = os.WriteFile(filepath.Join(tempFolder, "logs.json"), b, 0600)
 	assert.NoError(t, err)
 	time.Sleep(1 * time.Second)
@@ -186,6 +189,7 @@ func TestFileMixedSignals(t *testing.T) {
 	b = append(b, b2...)
 	b = append(b, '\n')
 	b = append(b, b3...)
+	b = append(b, '\n')
 	err = os.WriteFile(filepath.Join(tempFolder, "metrics.json"), b, 0600)
 	assert.NoError(t, err)
 	time.Sleep(1 * time.Second)

commit 1751b60422fe678d5ef49f3c8c0d5a2d9c7e9dbc

[chore][fileconsumer] Fix bug where flush period would be forgotten (#26033)
Adds a test for and fixes a bug introduced in
https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/25847

No changelog is necessary because the bug was introduced yesterday and
was never released.

pkg/stanza/fileconsumer/reader_factory.go
@@ -4,6 +4,7 @@
 package fileconsumer // import "github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/fileconsumer"
 
 import (
+	"bufio"
 	"os"
 	"path/filepath"
 	"runtime"
@@ -28,44 +29,52 @@ type readerFactory struct {
 }
 
 func (f *readerFactory) newReader(file *os.File, fp *fingerprint.Fingerprint) (*reader, error) {
+	lineSplitFunc, err := f.splitterFactory.Build(f.readerConfig.maxLogSize)
+	if err != nil {
+		return nil, err
+	}
 	return f.build(file, &readerMetadata{
 		Fingerprint:    fp,
 		FileAttributes: map[string]any{},
-	})
+	}, lineSplitFunc)
 }
 
 // copy creates a deep copy of a reader
 func (f *readerFactory) copy(old *reader, newFile *os.File) (*reader, error) {
+	var err error
+	lineSplitFunc := old.lineSplitFunc
+	if lineSplitFunc == nil {
+		lineSplitFunc, err = f.splitterFactory.Build(f.readerConfig.maxLogSize)
+		if err != nil {
+			return nil, err
+		}
+	}
 	return f.build(newFile, &readerMetadata{
 		Fingerprint:     old.Fingerprint.Copy(),
 		Offset:          old.Offset,
 		FileAttributes:  util.MapCopy(old.FileAttributes),
 		HeaderFinalized: old.HeaderFinalized,
-	})
+	}, lineSplitFunc)
 }
 
 func (f *readerFactory) newFingerprint(file *os.File) (*fingerprint.Fingerprint, error) {
 	return fingerprint.New(file, f.readerConfig.fingerprintSize)
 }
 
-func (f *readerFactory) build(file *os.File, m *readerMetadata) (r *reader, err error) {
+func (f *readerFactory) build(file *os.File, m *readerMetadata, lineSplitFunc bufio.SplitFunc) (r *reader, err error) {
 	r = &reader{
 		readerConfig:   f.readerConfig,
 		readerMetadata: m,
 		file:           file,
 		SugaredLogger:  f.SugaredLogger.With("path", file.Name()),
 		decoder:        decoder.New(f.encoding),
+		lineSplitFunc:  lineSplitFunc,
 	}
 
 	if !f.fromBeginning {
 		if err = r.offsetToEnd(); err != nil {
 			return nil, err
 		}
-	}
-
-	r.lineSplitFunc, err = f.splitterFactory.Build(f.readerConfig.maxLogSize)
-	if err != nil {
-		return nil, err
 	}
 
 	if f.headerConfig == nil || m.HeaderFinalized {

pkg/stanza/fileconsumer/reader_test.go
@@ -8,6 +8,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/decoder"
@@ -19,6 +20,41 @@ import (
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/operator/parser/regex"
 	"github.com/open-telemetry/opentelemetry-collector-contrib/pkg/stanza/testutil"
 )
+
+func TestPersistFlusher(t *testing.T) {
+	flushPeriod := 100 * time.Millisecond
+	sCfg := helper.NewSplitterConfig()
+	sCfg.Flusher.Period = flushPeriod
+	f, emitChan := testReaderFactoryWithSplitter(t, sCfg)
+
+	temp := openTemp(t, t.TempDir())
+	fp, err := f.newFingerprint(temp)
+	require.NoError(t, err)
+
+	r, err := f.newReader(temp, fp)
+	require.NoError(t, err)
+
+	_, err = temp.WriteString("log with newline\nlog without newline")
+	require.NoError(t, err)
+
+	// ReadToEnd will return when we hit eof, but we shouldn't emit the unfinished log yet
+	r.ReadToEnd(context.Background())
+	waitForToken(t, emitChan, []byte("log with newline"))
+
+	// Even trying again shouldn't produce the log yet because the flush period still hasn't expired.
+	r.ReadToEnd(context.Background())
+	expectNoTokensUntil(t, emitChan, 2*flushPeriod)
+
+	// A copy of the reader should remember that we last emitted about 200ms ago.
+	copyReader, err := f.copy(r, temp)
+	assert.NoError(t, err)
+
+	// This time, the flusher will kick in and we should emit the unfinished log.
+	// If the copy did not remember when we last emitted a log, then the flushPeriod
+	// will not be expired at this point so we won't see the unfinished log.
+	copyReader.ReadToEnd(context.Background())
+	waitForToken(t, emitChan, []byte("log without newline"))
+}
 
 func TestTokenization(t *testing.T) {
 	testCases := []struct {
@@ -198,8 +234,11 @@ func TestHeaderFingerprintIncluded(t *testing.T) {
 }
 
 func testReaderFactory(t *testing.T) (*readerFactory, chan *emitParams) {
+	return testReaderFactoryWithSplitter(t, helper.NewSplitterConfig())
+}
+
+func testReaderFactoryWithSplitter(t *testing.T, splitterConfig helper.SplitterConfig) (*readerFactory, chan *emitParams) {
 	emitChan := make(chan *emitParams, 100)
-	splitterConfig := helper.NewSplitterConfig()
 	enc, err := decoder.LookupEncoding(splitterConfig.Encoding)
 	require.NoError(t, err)
 	return &readerFactory{