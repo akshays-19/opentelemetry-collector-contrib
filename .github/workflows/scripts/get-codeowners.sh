akshays-19/opentelemetry-collector-contrib

commit 596a66b96bc5cfeee1339d4195a174c0e5d541ad

[chore][CI/CD] Fix bugs in get-codeowners.sh (#29746)
**Description:** <Describe what has changed.>
<!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
Ex. Adding a feature - Explain what this achieves.-->
There's a combination of bugs currently existing in `get-codeowners.sh`
and the `Mark issues as stale` workflow. This implements proposed
solution 2 of #29744
- Match component against beginning of line in code owners.

**Issues**
Resolves #29744

**Testing**
Basic spot checking script:
```
COMPONENTS=("azure" "processor/resourcedetection" "processor/resourcedetectionprocessor" "test" "testbed" "extension/observer" "internal/k8stest" "extension/encoding" "flaky" "pkg/translator" "tools" )

for EXAMPLE_COMPONENT in ${COMPONENTS[@]}; do
    OWNERS=$(COMPONENT=${EXAMPLE_COMPONENT} bash ".github/workflows/scripts/get-codeowners.sh")
    echo $EXAMPLE_COMPONENT: $OWNERS
done
```
Output:
```
azure:
processor/resourcedetection: @Aneurysm9 @dashpole
processor/resourcedetectionprocessor: @Aneurysm9 @dashpole
test:
testbed: @open-telemetry/collector-approvers
extension/observer: @dmitryax @rmfitzpatrick
internal/k8stest: @crobert-1
extension/encoding: @atoulme @dao-jun @dmitryax @MovieStoreGuy @VihasMakwana
flaky:
pkg/translator:
tools:
```
Reference
[CODEOWNERS](https://github.com/open-telemetry/opentelemetry-collector-contrib/blob/main/.github/CODEOWNERS)
as source of truth.

---------

Co-authored-by: Evan Bradley <11745660+evan-bradley@users.noreply.github.com>

.github/workflows/scripts/get-codeowners.sh
@@ -9,8 +9,14 @@
 
 set -euo pipefail
 
+get_component_type() {
+  echo "${COMPONENT}" | cut -f 1 -d '/'
+}
+
 get_codeowners() {
-  echo "$((grep -m 1 "${1}" .github/CODEOWNERS || true) | sed 's/   */ /g' | cut -f3- -d ' ')"
+  echo "$((grep -m 1 "^${1}/\s" .github/CODEOWNERS || true) | \
+        sed 's/   */ /g' | \
+        cut -f3- -d ' ')"
 }
 
 if [[ -z "${COMPONENT:-}" ]]; then
@@ -26,14 +32,12 @@ RESULT=$(grep -c "${COMPONENT}" .github/CODEOWNERS || true)
 # if so, try to narrow things down by appending the component
 # or a forward slash to the label.
 if [[ ${RESULT} != 1 ]]; then
-    COMPONENT_TYPE=$(echo "${COMPONENT}" | cut -f 1 -d '/')
+    COMPONENT_TYPE=$(get_component_type "${COMPONENT}")
     OWNERS="$(get_codeowners "${COMPONENT}${COMPONENT_TYPE}")"
+fi
 
-    if [[ -z "${OWNERS:-}" ]]; then
-        OWNERS="$(get_codeowners "${COMPONENT}/")"
-    fi
-else
-    OWNERS="$(get_codeowners $COMPONENT)"
+if [[ -z "${OWNERS:-}" ]]; then
+    OWNERS="$(get_codeowners "${COMPONENT}")"
 fi
 
 echo "${OWNERS}"

commit 146e43cfa33bb3da7305d93ef38d0be83b4f667e

[chore][CI/CD] Ping code owners fix (#29849)
**Description:** <Describe what has changed.>
<!--Ex. Fixing a bug - Describe the bug and how this fixes the issue.
Ex. Adding a feature - Explain what this achieves.-->
A bug was introduced in #29746 where we did an explicit match on the
entire component name. This resulted in never having multiple matches
for a single component. We only would check for a component type
appended to the component if there were multiple results, but we still
need to check with the component type appended to the end, regardless of
results.

**Testing:** <Describe what testing was performed and which tests were
added.>
Found in
https://github.com/open-telemetry/opentelemetry-collector-contrib/issues/29845.
I've added every valid component determined by `get-components.sh` to my
test, as well as some invalid ones.
```
flaky:
test:
azure:
tools:
cmd/configschema: @mx-psi @dmitryax @pmcollins
cmd/githubgen: @atoulme
cmd/mdatagen: @dmitryax
cmd/opampsupervisor: @evan-bradley @atoulme @tigrannajaryan
cmd/otelcontribcol:
cmd/oteltestbedcol:
cmd/telemetrygen: @mx-psi @codeboten
confmap/provider/s3provider: @Aneurysm9
connector/countconnector: @djaglowski @jpkrohling
connector/datadogconnector: @mx-psi @gbbr @dineshg13
connector/exceptionsconnector: @jpkrohling @marctc
connector/failoverconnector: @djaglowski @fatsheep9146
connector/routingconnector: @jpkrohling @mwear
connector/servicegraphconnector: @jpkrohling @mapno
connector/spanmetricsconnector: @albertteoh
examples/demo: @open-telemetry/collector-approvers
exporter/alertmanagerexporter: @jpkrohling @sokoide @mcube8
exporter/alibabacloudlogserviceexporter: @shabicheng @kongluoxing @qiansheng91
exporter/awscloudwatchlogsexporter: @boostchicken @bryan-aguilar @rapphil
exporter/awsemfexporter: @Aneurysm9 @shaochengwang @mxiamxia @bryan-aguilar
exporter/awskinesisexporter: @Aneurysm9 @MovieStoreGuy
exporter/awss3exporter: @atoulme @pdelewski

.github/workflows/scripts/get-codeowners.sh
@@ -14,7 +14,13 @@ get_component_type() {
 }
 
 get_codeowners() {
-  echo "$((grep -m 1 "^${1}/\s" .github/CODEOWNERS || true) | \
+  # grep arguments explained:
+  #   -m 1: Match the first occurrence
+  #   ^: Match from the beginning of the line
+  #   ${1}: Insert first argument given to this function
+  #   [\/]\?: Match 0 or 1 instances of a forward slash
+  #   \s: Match any whitespace character
+  echo "$((grep -m 1 "^${1}[\/]\?\s" .github/CODEOWNERS || true) | \
         sed 's/   */ /g' | \
         cut -f3- -d ' ')"
 }
@@ -24,20 +30,11 @@ if [[ -z "${COMPONENT:-}" ]]; then
     exit 1
 fi
 
-# grep exits with status code 1 if there are no matches,
-# so we manually set RESULT to 0 if nothing is found.
-RESULT=$(grep -c "${COMPONENT}" .github/CODEOWNERS || true)
-
-# there may be more than 1 component matching a label
-# if so, try to narrow things down by appending the component
-# or a forward slash to the label.
-if [[ ${RESULT} != 1 ]]; then
-    COMPONENT_TYPE=$(get_component_type "${COMPONENT}")
-    OWNERS="$(get_codeowners "${COMPONENT}${COMPONENT_TYPE}")"
-fi
+OWNERS="$(get_codeowners "${COMPONENT}")"
 
 if [[ -z "${OWNERS:-}" ]]; then
-    OWNERS="$(get_codeowners "${COMPONENT}")"
+    COMPONENT_TYPE=$(get_component_type "${COMPONENT}")
+    OWNERS="$(get_codeowners "${COMPONENT}${COMPONENT_TYPE}")"
 fi
 
 echo "${OWNERS}"